2c48eacb3e11252efcfc85823bcdc497a21c88a5
nia-vf <102545622+nia-vf@users.noreply.github.com>
2022-10-15 11:43:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-import http from 'http'
+import http from 'http';
+import url from 'url';
+import fs from 'fs';
 
 http.createServer(function(request, response) {
-    response.writeHeader(200, { "Content-Type": "text/plain" });
-    response.write("Node JS Running on EC2");
-    response.end();
+    var path = url.parse(request.url).pathname;
+    switch (path) {
+        case "/":
+            response.writeHeader(200, { "Content-Type": "text/plain" });
+            response.end();
+            break;
+        case "/HtmlPage1.html":
+            fs.readFile(__dirname + path, function(error, data) {
+                if (error) {
+                    response.writeHead(404);
+                    response.write(error);
+                    response.end();
+                } else {
+                    response.writeHead(200, { "Content-Type": "text/html" });
+                    response.write(data);
+                    response.end();
+                }
+            });
+            break;
+        case "HtmlPage2.html":
+            fs.readFile(__dirname + path, function(error, data) {
+                if (error) {
+                    response.writeHead(404);
+                    response.write(error);
+                    response.end();
+                } else {
+                    response.writeHead(200, { "Content-Type": "text/html" });
+                    response.write(data);
+                    response.end();
+                }
+            });
+            break;
+        default:
+            response.writeHead(404);
+            response.write("Oops this doesn't exist - 404");
+            response.end();
+            break;
+    }
 }).listen(80);
